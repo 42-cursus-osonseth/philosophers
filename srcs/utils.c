9d4fa6b50248a8b9afa7eacb7b9c655fc0400d0a
Osonseth <max.cesaria@gmail.com>
2024-09-18 18:27:44 +0200

    add: protect overflow in ft_atoi with ft_strlen

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,37 +6,52 @@
 /*   By: max <max@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 12:53:26 by max               #+#    #+#             */
-/*   Updated: 2024/09/02 14:52:23 by max              ###   ########.fr       */
+/*   Updated: 2024/09/18 18:25:55 by max              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int ft_strlen(char *str)
+{
+	if (!str)
+		return 0;
+	int i;
+	i = 0;
+
+	while (str[i])
+		i++;
+	return i;
+}
+
 long int get_timestamp_in_ms()
 {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
 }
 long int ft_atoi(char *str)
 {
 	long int result;
-	
+	int len;
+
+	len = ft_strlen(str);
 	result = 0;
 	while (*str)
 	{
 		result = result * 10 + (*str - '0');
 		str++;
 	}
-	if (result < 0 || result > INT_MAX)
+
+	if (result < 0 || result > INT_MAX || len > 10)
 		return OVERFLOW;
 	return (result);
 }
 bool philosopher_is_dead(t_philosopher *philosopher)
 {
-    bool is_dead;
-    pthread_mutex_lock(&philosopher->shared_data->death);
-    is_dead = philosopher->shared_data->philosopher_is_dead;
-    pthread_mutex_unlock(&philosopher->shared_data->death);
-    return is_dead;
+	bool is_dead;
+	pthread_mutex_lock(&philosopher->shared_data->death);
+	is_dead = philosopher->shared_data->philosopher_is_dead;
+	pthread_mutex_unlock(&philosopher->shared_data->death);
+	return is_dead;
 }
